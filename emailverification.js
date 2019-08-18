af07fca0de95f2a3f84323e4c09f6fb6de87b7fb
Nwani Victory <vickywane@gmail.com>
2019-08-18 22:49:15 +0100

    Added Start script

--- emailverification.js
+++ emailverification.js
@@ -1,15 +1,27 @@
+//recieve and send welcome email to users
+require('@google/cloud-debug');
+
 var api_key = 'XXXXXXXXXXXXXXXXXXXXXXX';
 var domain = 'www.mydomain.com';
-var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
- 
-var data = {
-  from: 'Excited User <me@samples.mailgun.org>',
-  to: 'serobnic@mail.ru',
-  subject: 'Hello',
-  text: 'Testing some Mailgun awesomeness!'
+var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
+
+exports.emailVerification = function(req, _res) {
+  let sender = 'vickywane@gmail.com';
+  let reciever = req.query.email;
+
+  let account_type = req.query.account_type;
+
+  var data = {
+    from: sender,
+    to: reciever,
+    subject: 'Verify Email for ' + account_type + 'Remotify Account',
+    text: 'Testing some Mailgun awesomeness!',
+  };
+
+  mailgun.messages().send(data, function(error, body) {
+    console.log(body);
+    if (error) {
+      console.log(error);
+    }
+  });
 };
- 
-mailgun.messages().send(data, function (error, body) {
-  console.log(body);
-});
- 

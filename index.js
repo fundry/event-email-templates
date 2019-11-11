4448dedd27238787658c5f4e235167f56727c7de
Nwani Victory <vickywane@gmail.com>
2019-11-11 17:57:34 +0100

    Added pug for template generation

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 require('dotenv').config();
 //recieve and send welcome email to users
 
-var api_key =' process.env.MAILGUN_API';
-var domain =  'process.env.MAILGUN_SANDBOX';
+var api_key = process.env.MAILGUN_API;
+var domain = process.env.MAILGUN_SANDBOX;
 var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
 
 exports.emailVerification = function(req, _res) {
-  let sender = 'vickywane@gmail.com';
+  let sender = process.env.SENDER;
   let reciever = req.query.email;
 
   let account_type = req.query.account_type;
 
+  console.log(api_key, domain);
+  console.log(reciever, account_type); // try testing with postman if it doesnt CURL
+  /*
   var data = {
     from: sender,
     to: reciever,
@@ -23,9 +26,5 @@ exports.emailVerification = function(req, _res) {
     if (error) {
       console.log(error);
     }
-  });
+  }); */
 };
-
-// exports.emailVerification = (req, res) => {
-//   res.send('Hello, World');
-// };

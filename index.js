f79485b87cc3bd39103b9ff297b227dede4d0cc6
Nwani Victory <vickywane@gmail.com>
2019-11-12 11:07:20 +0100

    Welcome email template

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 require('dotenv').config();
-//recieve and send welcome email to users
-
 var api_key = process.env.MAILGUN_API;
 var domain = process.env.MAILGUN_SANDBOX;
 var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
+const nodemailer = require('nodemailer');
 
 exports.emailVerification = function(req, _res) {
   let sender = process.env.SENDER;
   let reciever = req.query.email;
-
   let account_type = req.query.account_type;
 
   console.log(api_key, domain);
-  console.log(reciever, account_type); // try testing with postman if it doesnt CURL
+  console.log(reciever, account_type); // try testing with postman if it doesnt work with CURL
+
+  var transport = nodemailer.createTransport({
+    host: domain,
+    port: 587,
+    secure: false,
+    tls: { ciphers: 'SSLv3' },
+    auth: {
+      user: api_key,
+      pass: '<Mailgun SMTP password>',
+    },
+  });
+
+  transport.sendMail(
+    {
+      from: sender,
+      to: reciever,
+      subject: ' Verify Email Address',
+      text: 'Verify your registered mail for you workspace',
+      html: { path: 'dist/welcome.template.html' },
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+
   /*
   var data = {
     from: sender,
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
-<h1> Remotify Cloud Function </h1>
+<h1> Remotify Cloud Functions </h1>
 
-<p> This repository contains all source codes for cloud functions working for Remotify </p>
+<h3 style={{textAlign : 'center}} > This repository contains all source codes for cloud functions working for the Remotify project </h3>
+<hr / >
+
+<p>  A sample test for this functions using CURL is  <code>http://localhost:8080/?email=EMAIL&account_type=ORGANIZATION_TYPE</code>   </p>
+
+
+

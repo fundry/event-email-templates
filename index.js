8f582f0c3eda9f7d6ab2dfd3508e0d45a8a08df7
Nwani Victory <vickywane@gmail.com>
2019-11-12 20:27:39 +0100

    Setup nodemailer - mailgun pipeline

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 require('dotenv').config();
 var api_key = process.env.MAILGUN_API;
 var domain = process.env.MAILGUN_SANDBOX;
-var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
 const nodemailer = require('nodemailer');
+
+const username = process.env.SMTP_USERNAME;
+const password = process.env.SMTP_PASSWORD;
 
 exports.emailVerification = function(req, _res) {
   let sender = process.env.SENDER;
   let reciever = req.query.email;
   let account_type = req.query.account_type;
 
-  console.log(api_key, domain);
-  console.log(reciever, account_type); // try testing with postman if it doesnt work with CURL
+  console.log(username);
+  console.log(password);
+  console.log(reciever, account_type); // test with postman
 
   var transport = nodemailer.createTransport({
-    host: domain,
+    host: process.env.HOST,
     port: 587,
     secure: false,
-    tls: { ciphers: 'SSLv3' },
     auth: {
-      user: api_key,
-      pass: '<Mailgun SMTP password>',
+      user: username,
+      pass: password,
     },
   });
 
@@ -31,23 +33,13 @@ exports.emailVerification = function(req, _res) {
       text: 'Verify your registered mail for you workspace',
       html: { path: 'dist/welcome.template.html' },
     },
-    (error) => {
-      console.log(error);
+    (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('message sent');
+      }
+      transport.close();
     }
   );
-
-  /*
-  var data = {
-    from: sender,
-    to: reciever,
-    subject: 'Verify Email for ' + account_type + 'Remotify Account',
-    text: 'Testing some Mailgun awesomeness!',
-  };
-
-  mailgun.messages().send(data, function(error, body) {
-    console.log(body);
-    if (error) {
-      console.log(error);
-    }
-  }); */
 };
